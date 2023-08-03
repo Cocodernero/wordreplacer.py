f5e7a51d0326a899f05f74ca5b927b6368ec9ebe
Cocodernero <119940289+Cocodernero@users.noreply.github.com>
2023-08-03 14:24:35 +0300

    Add files via upload

---
+++ wordreplacer.py
@@ -0,0 +1,11 @@
+# a python code that can be used to replace errors in sentences 
+
+def replace_word():
+
+    str = "In Kenya, you will never lack problems to solve"
+    word_to_replace = input("Enter the word to replace:")
+    word_replacemet = input("Enter the word replacement:")
+    
+    print(str.replace(word_to_replace,word_replacemet))
+
+replace_word()
